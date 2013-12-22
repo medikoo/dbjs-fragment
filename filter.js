b48650999433fde18607df8cc8ae66f1fee9d2b4
Mariusz Nowak <mariusz@medikoo.com>
2013-12-22 16:41:46 +0100

    Fragment filter

---
+++ filter.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var assign         = require('es5-ext/object/assign')
+  , setPrototypeOf = require('es5-ext/object/set-prototype-of')
+  , callable       = require('es5-ext/object/valid-callable')
+  , d              = require('d/d')
+  , autoBind       = require('d/auto-bind')
+  , Set            = require('observable-set/create-read-only')(
+	require('observable-set/primitive')
+)
+  , serialize      = require('dbjs/_setup/serialize/object')
+
+  , hasOwnProperty = Object.prototype.hasOwnProperty
+  , create = Object.create, defineProperties = Object.defineProperties
+  , Fragment;
+
+module.exports = Fragment = function (fragment, filter) {
+	if (!(this instanceof Fragment)) return new Fragment(fragment, filter);
+	callable(filter);
+	Set.call(this);
+	defineProperties(this, {
+		__fragment__: d('', fragment),
+		__filter__: d('', filter),
+		__validated__: d('', create(null))
+	});
+	fragment.forEach(function (obj) {
+		this._onUpdate(obj._lastOwnEvent_);
+	}, this);
+	fragment.on('update', this._onUpdate);
+	fragment.on('delete', this._onDelete);
+};
+setPrototypeOf(Fragment, Set);
+
+Fragment.prototype = create(Set.prototype, assign({
+	constructor: d(Fragment)
+}, autoBind({
+	_serialize: d(serialize),
+	_onUpdate: d(function (event) {
+		var obj = event.object, id = obj.__id__;
+		if (!hasOwnProperty.call(this.__validated__, id)) {
+			if (!(this.__validated__[id] = Boolean(this.__filter__(obj)))) return;
+		} else if (!this.__validated__[id]) {
+			return;
+		}
+		this._add(obj);
+		this.emit('update', event);
+	}),
+	_onDelete: d(function (id) {
+		var obj = this.__setData__[id];
+		if (!obj) return;
+		this._delete(obj);
+		this.emit('delete', id);
+	})
+})));
---
+++ test/filter.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var toArray    = require('es6-iterator/to-array')
+  , Database   = require('dbjs')
+  , objectFrag = require('../object-family')
+
+  , getId = function (obj) { return obj.__id__; };
+
+module.exports = function (t, a) {
+	var db = new Database(), Type1, Type2, Type3, obj11, obj21, obj31, obj32
+	  , iterator, updates, deletes, frag;
+
+	Type3 = db.Object.extend('FragFilterTest3',
+		{ iteRemtest: { type: db.String } });
+	Type1 = db.Object.extend('FragFilterTest1', {
+		iteTestStr: { type: db.String },
+		iteTestMulti: { type: db.String, multiple: true },
+		otherObj: { type: Type3 },
+		otherMultipleObj: { type: Type3, multiple: true }
+	});
+	Type2 = db.Object.extend('FragFilterTest2', {
+		iteTest: { type: Type1, reverse: 'iteRev1' }
+	});
+
+	obj31 = new Type3({ iteRemtest: 'remotes' });
+	obj11 = new Type1({ iteTestStr: 'foo', iteTestMulti: ['raz', 'dwa'],
+		otherObj: obj31 });
+	obj21 = new Type2({ iteTest: obj11 });
+
+	frag = objectFrag(obj11);
+
+	iterator = t(frag, function (obj) { return obj.__id__.indexOf('*') === -1; });
+	a.deep(toArray(iterator).map(getId).sort(), [obj11, obj11.$iteTestStr,
+		obj11.$otherObj, obj31, obj31.$iteRemtest, obj21,
+		obj21.$iteTest].map(getId).sort(), "Objects");
+	updates = [];
+	iterator.on('update', function (event) {
+		updates.push(event.object.__id__);
+	});
+	deletes = [];
+	iterator.on('delete', function (id) { deletes.push(id); });
+
+	obj11.otherObj = null;
+	a.deep(updates, [obj11.$otherObj.__id__], "Clear existing: Updates");
+	updates.length = 0;
+	a.deep(deletes.sort(), [obj31, obj31.$iteRemtest].map(getId).sort(),
+			"Clear existing: Deletes");
+	deletes.length = 0;
+
+	obj21.iteTest = null;
+	a.deep(updates, [obj21.$iteTest.__id__], "Remove reverse: Updates");
+	updates.length = 0;
+	a.deep(deletes.sort(), [obj21, obj21.$iteTest].map(getId).sort(),
+		"Remove reverse: Deletes");
+	deletes.length = 0;
+
+	obj11.otherMultipleObj.add(obj31);
+	a.deep(updates.sort(), [obj31, obj31.$iteRemtest].map(getId).sort(),
+		"Add obj item: Updates");
+	updates.length = 0;
+	a.deep(deletes, [], "Add obj item: Deletes");
+	deletes.length = 0;
+
+	obj11.otherMultipleObj.delete(obj31);
+	a.deep(updates, [], "Delete obj item: Updates");
+	updates.length = 0;
+	a.deep(deletes.sort(), [obj31, obj31.$iteRemtest].map(getId).sort(),
+		"Delete obj item: Deletes");
+	deletes.length = 0;
+
+	obj32 = new Type3();
+	obj11.otherMultipleObj.delete(obj32);
+	a.deep(updates, [], "Invoke delete item: Updates");
+	updates.length = 0;
+	a.deep(deletes, [], "Invoke delete item: Deletes");
+	deletes.length = 0;
+};
